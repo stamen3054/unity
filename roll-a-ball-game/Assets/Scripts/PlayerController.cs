b253c400a21521c8f96d6f06de0ae0bfe0601f35
Wang <yw050121@cerner.com>
2018-07-02 14:49:52 -0500

    finished roll a ball game

--- roll-a-ball-game/Assets/Scripts/PlayerController.cs
+++ roll-a-ball-game/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
     private Rigidbody rb;
+    private int count;
+    public Text countText;
+    public Text winText;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        countText.text = "Score: " + count.ToString();
+        winText.text = "";
     }
 
     private void FixedUpdate()
@@ -20,6 +27,16 @@ public class PlayerController : MonoBehaviour {
         Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
 
         rb.AddForce(movement * speed);
+    }
 
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        count++;
+        countText.text = "Score: " + count.ToString();
+        if(count>=24)
+        {
+            winText.text = "You have collected all cubes!";
+        }
     }
 }
